d9fbe28632a3dae0d37e91817c634ec09188d57b
Afnanda Muhammad Iqbal Saputra <afnanda.iqbal@gmail.com>
2021-06-14 04:12:16 -0700

    cok

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'abd35d75599d79a2f9e2b9f8f98413b7'),
-    },
-  },
-});
--- my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '4d908a09-7f38-491f-b37e-75d53e45f4e6'
-};
